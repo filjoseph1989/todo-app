357a9fbc8f79ce89aee0d16380885ed414707b71
Fil <filjoseph22@gmail.com>
2024-06-13 11:52:30 +0800

    defined an initial user query class

---
+++ app/GraphQL/Queries/UsersQuery.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use Closure;
+use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\SelectFields;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class UsersQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'users',
+        'description' => 'A query'
+    ];
+
+    public function type(): Type
+    {
+        return Type::nonNull(Type::listOf(Type::nonNull(GraphQL::type('User'))));
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::id(),
+            ],
+            'email' => [
+                'name' => 'email',
+                'type' => Type::string(),
+            ],
+        ];
+    }
+
+    public function resolve($root, array $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields)
+    {
+        if (isset($args['id'])) {
+            return User::where('id', $args['id'])->get();
+        }
+        if (isset($args['email'])) {
+            return User::where('email', $args['email'])->get();
+        }
+
+        return User::all();
+    }
+}
