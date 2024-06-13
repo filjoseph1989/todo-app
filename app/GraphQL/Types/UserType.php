b4c04498020cac25a33daafca2105a3fbdf87d37
Fil <filjoseph22@gmail.com>
2024-06-13 17:25:31 +0800

    improved user type by adding new resolve method

--- app/GraphQL/Types/UserType.php
+++ app/GraphQL/Types/UserType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Types;
 
+use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Type as GraphQLType;
 
@@ -14,32 +15,61 @@ class UserType extends GraphQLType
         'description' => 'A type'
     ];
 
+    /**
+     * Returns an array containing the fields of the UserType
+     *
+     * @return array
+     */
     public function fields(): array
     {
         return [
             'id' => [
                 'type' => Type::nonNull(Type::id()),
                 'description' => 'The id of the user',
-                // Use 'alias', if the database column is different from the type name.
-                // This is supported for discrete values as well as relations.
-                // - you can also use `DB::raw()` to solve more complex issues
-                // - or a callback returning the value (string or `DB::raw()` result)
                 'alias' => 'user_id',
-                'resolve' => function ($root, array $args) {
-                    // If you want to resolve the field yourself,
-                    // it can be done here
+                'resolve' => function ($root, $args) {
                     return $root->id;
-                }
+                },
             ],
             'email' => [
                 'type' => Type::string(),
                 'description' => 'The email of user',
-                'resolve' => function ($root, array $args) {
-                    // If you want to resolve the field yourself,
-                    // it can be done here
-                    return strtolower($root->email);
-                }
+            ],
+            'first_name' => [
+                'type' => Type::string(),
+                'description' => 'The first name of user',
+            ],
+            'last_name' => [
+                'type' => Type::string(),
+                'description' => 'The last name of user',
+            ],
+            'email_verified_at' => [
+                'type' => Type::string(),
+                'description' => 'The email verified at of user',
             ],
         ];
+    }
+
+    /**
+     * Resolves the field based on the root, arguments, context and the information about the field.
+     *
+     * @param mixed $root The source object on which the field is being resolved.
+     * @param mixed[] $args The arguments passed to the field.
+     * @param mixed $context The context passed to the field resolver.
+     * @param ResolveInfo $info The information about the field.
+     * @return string|null The resolved value of the field.
+     */
+    public function resolve($root, $args, $context, ResolveInfo $info): ?string
+    {
+        $field = $info->fieldName;
+
+        switch ($field) {
+            case 'id':
+            case 'first_name':
+            case 'last_name':
+            case 'email':
+            case 'email_verified_at':
+                return $root->{$field};
+        }
     }
 }
