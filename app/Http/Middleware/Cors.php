1d24876017dbe0546990d0936af1f400069d7f23
Fil <filjoseph22@gmail.com>
2024-06-15 18:31:45 +0800

    handle cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,7 +13,7 @@ class Cors
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
 
