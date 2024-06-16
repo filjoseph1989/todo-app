acac7084174fc311418fccc558363ea0d8eb9a63
Fil <filjoseph22@gmail.com>
2024-06-17 00:08:44 +0800

    removed login type and mutation because it was moved to another schema

--- app/GraphQL/Schemas/DefaultSchema.php
+++ app/GraphQL/Schemas/DefaultSchema.php
@@ -4,11 +4,9 @@ namespace App\GraphQL\Schemas;
 
 use App\GraphQL\Mutations\CreateTaskMutation;
 use App\GraphQL\Mutations\DeleteTaskMutation;
-use App\GraphQL\Mutations\LoginMutation;
 use App\GraphQL\Mutations\UpdateTaskMutation;
 use App\GraphQL\Queries\TaskQuery;
 use App\GraphQL\Queries\UsersQuery;
-use App\GraphQL\Types\LoginType;
 use App\GraphQL\Types\UserType;
 use App\GraphQL\Types\TaskType;
 use Rebing\GraphQL\Support\Contracts\ConfigConvertible;
@@ -21,7 +19,6 @@ class DefaultSchema implements ConfigConvertible
             'types' => [
                 UserType::class,
                 TaskType::class,
-                LoginType::class,
             ],
             'query' => [
                 UsersQuery::class,
@@ -31,7 +28,6 @@ class DefaultSchema implements ConfigConvertible
                 UpdateTaskMutation::class,
                 CreateTaskMutation::class,
                 DeleteTaskMutation::class,
-                LoginMutation::class,
             ],
             'method' => ['GET', 'POST'],
         ];
