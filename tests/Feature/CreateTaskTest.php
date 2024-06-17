743e64e646d376a825bbd11835e98aa9096f909e
Fil <filjoseph22@gmail.com>
2024-06-17 08:29:47 +0800

    added create task

---
+++ tests/Feature/CreateTaskTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use DB;
+use App\Models\User;
+
+describe('UpdateTaskTest', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create();
+
+        $query = <<<GQL
+            mutation Login {
+                login(email: "{$this->user->email}", password: "password") {
+                    token
+                }
+            }
+        GQL;
+
+        $response = $this->postJson('/graphql/auth', [
+            'query' => $query,
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->token = $response->json('data.login.token');
+    });
+
+    afterEach(function () {
+        User::where('email', $this->user->email)->delete();
+    });
+
+    it('can create a task', function () {
+        DB::beginTransaction();
+
+        try {
+            $query = <<<GQL
+                mutation CreateTask {
+                    createTask(task: "Creating new task", user_id: "1") {
+                        id
+                        task
+                        status
+                    }
+                }
+            GQL;
+
+            $response = $this->postJson('/graphql', [
+                'query' => $query,
+            ], [
+                'Authorization' => 'Bearer ' . $this->token,
+            ]);
+
+            $response->assertStatus(200);
+
+            $data = $response->json('data.createTask');
+
+            $this->assertDatabaseHas('tasks', [
+                'task' => $data['task'],
+                'status' => 'todo',
+            ]);
+        } finally {
+            DB::rollBack();
+        }
+    });
+});
